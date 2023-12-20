d7d3ba5f9b3a67d1bdedd58105080c8a56e9ff72
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-12-20 14:23:27 +0300

    Done with assignment

--- lib/debug.py
+++ lib/debug.py
@@ -3,6 +3,13 @@
 from config import CONN, CURSOR
 from song import Song
 
+def reset_database():
+    Song.drop_table()
+    Song.create_table()
+    Song.create("hello", "25")
+    Song.create("99 Problems", "The Black Album")   
+
 
 if __name__ == '__main__':
+    reset_database()
     import ipdb; ipdb.set_trace()
--- lib/song.py
+++ lib/song.py
@@ -2,6 +2,8 @@ from config import CONN, CURSOR
 
 class Song:
     
+    all=[]
+
     def __init__(self, name, album):
         self.id = None
         self.name = name
@@ -18,17 +20,53 @@ class Song:
             """
         
         CURSOR.execute(sql)
+    
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS songs
+        """
+
+        CURSOR.execute(sql)
 
     def save(self):
         sql = """
         INSERT INTO songs (name, album) VALUES(?, ?)
         """
         CURSOR.execute(sql, (self.name, self.album))
+        CONN.commit()
         self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
 
     @classmethod
     def create(cls, name, album):
         song = Song(name, album)
         song.save()
+        return song 
+
+    @classmethod 
+    def new_from_db(cls, row):
+        song = cls(row[1], row[2])
+        song.id = row[0]
         return song
+
+    @classmethod 
+    def get_all(cls):
+        sql = """
+        SELECT * FROM songs
+        """
+        all = CURSOR.execute(sql).fetchall()
+        cls.all = [cls.new_from_db(row) for row in all]
+        return cls.all
+
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT * FROM songs 
+            WHERE name = ?
+            LIMIT 1
+        """
+
+        song = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.new_from_db(song)
+    
 
